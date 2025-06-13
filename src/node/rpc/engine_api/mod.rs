e7b58497b25ca682d116caa6d5e7e0c8742c3ce9
Rez <rez@berachain.com>
2025-06-13 10:28:34 +1000

    Add Engine RPCs and now works with BeaconKit (#1)

--- src/node/rpc/engine_api/mod.rs
+++ src/node/rpc/engine_api/mod.rs
@@ -1,8 +1,6 @@
-use jsonrpsee_core::server::RpcModule;
 use std::sync::Arc;
 use reth::api::{AddOnsContext, FullNodeComponents, NodeTypes};
-use reth::rpc::api::IntoEngineApiRpcModule;
-use reth_node_builder::rpc::{EngineApiBuilder, EngineValidatorBuilder};
+use reth_node_builder::rpc::EngineValidatorBuilder;
 use reth_node_ethereum::EthereumEngineValidator;
 use crate::node::BerachainNode;
 
@@ -27,26 +25,44 @@ where
     }
 }
 
-#[derive(Debug, Default)]
-#[non_exhaustive]
-pub struct BerachainEngineApi;
+// #[derive(Debug, Default)]
+// #[non_exhaustive]
+// pub struct BerachainEngineApi;
+//
+// impl IntoEngineApiRpcModule for BerachainEngineApi {
+//     fn into_rpc_module(self) -> RpcModule<()> {
+//         todo!("implement")
+//         RpcModule::new(())
+//     }
+// }
+//
+// #[derive(Debug, Default)]
+// pub struct BerachainEngineApiBuilder;
+//
+// impl<N> EngineApiBuilder<N> for BerachainEngineApiBuilder
+// where
+//     N: FullNodeComponents,
+// {
+//     type EngineApi = BerachainEngineApi;
+//
+//     async fn build_engine_api(self, _ctx: &AddOnsContext<'_, N>) -> eyre::Result<Self::EngineApi> {
+//         Ok(BerachainEngineApi::default())
+//     }
+// }
 
-impl IntoEngineApiRpcModule for BerachainEngineApi {
-    fn into_rpc_module(self) -> RpcModule<()> {
-        RpcModule::new(())
-    }
-}
-
-#[derive(Debug, Default)]
-pub struct BerachainEngineApiBuilder;
-
-impl<N> EngineApiBuilder<N> for BerachainEngineApiBuilder
-where
-    N: FullNodeComponents,
-{
-    type EngineApi = BerachainEngineApi;
-
-    async fn build_engine_api(self, _ctx: &AddOnsContext<'_, N>) -> eyre::Result<Self::EngineApi> {
-        Ok(BerachainEngineApi::default())
-    }
-}
+// /// Builder for [`EthereumEngineValidator`].
+// #[derive(Debug, Default, Clone)]
+// #[non_exhaustive]
+// pub struct BerachainEngineValidatorBuilder;
+//
+// impl<Node, Types> EngineValidatorBuilder<Node> for BerachainEngineValidatorBuilder
+// where
+//     Types: NodeTypes<ChainSpec = BerachainChainSpec, Payload = EthEngineTypes, Primitives = EthPrimitives>,
+//     Node: FullNodeComponents<Types = Types>,
+// {
+//     type Validator = EthereumEngineValidator;
+//
+//     async fn build(self, ctx: &AddOnsContext<'_, Node>) -> eyre::Result<Self::Validator> {
+//         Ok(EthereumEngineValidator::new(ctx.config.chain.inner().clone().into()))
+//     }
+// }
