c911671b9b69cc24faec76e1e52c237960fd1e68
Rez <rez@berachain.com>
2025-06-20 05:48:55 +1000

    chore: remove unused berachain_payload_id function and BerachainPayloadTypes
    Removed unused code identified during code review:
    - berachain_payload_id function was never called
    - BerachainPayloadTypes struct was defined but node still uses EthereumNode payload types
    - Removed sha2 dependency which was only used by the unused function

--- src/payload/mod.rs
+++ src/payload/mod.rs
@@ -3,12 +3,9 @@ pub mod builder;
 use alloy_eips::eip4895::{Withdrawal, Withdrawals};
 use alloy_primitives::B256;
 use alloy_rpc_types_engine::PayloadId;
-use reth_ethereum_engine_primitives::EthPayloadTypes;
 use reth_payload_builder::EthPayloadBuilderAttributes;
-use reth_payload_primitives::{PayloadAttributes, PayloadBuilderAttributes, PayloadTypes};
-use reth_primitives::EthPrimitives;
+use reth_payload_primitives::{PayloadAttributes, PayloadBuilderAttributes};
 use serde::{Deserialize, Serialize};
-use std::marker::PhantomData;
 
 pub use builder::BerachainPayloadBuilder;
 
@@ -78,34 +75,6 @@ impl PayloadBuilderAttributes for BerachainPayloadBuilderAttributes {
     type RpcPayloadAttributes = BerachainPayloadAttributes;
     type Error = <EthPayloadBuilderAttributes as PayloadBuilderAttributes>::Error;
 
-    fn payload_id(&self) -> PayloadId {
-        self.payload_attributes.payload_id()
-    }
-
-    fn parent(&self) -> B256 {
-        self.payload_attributes.parent()
-    }
-
-    fn timestamp(&self) -> u64 {
-        self.payload_attributes.timestamp()
-    }
-
-    fn suggested_fee_recipient(&self) -> alloy_primitives::Address {
-        self.payload_attributes.suggested_fee_recipient()
-    }
-
-    fn prev_randao(&self) -> B256 {
-        self.payload_attributes.prev_randao()
-    }
-
-    fn withdrawals(&self) -> &Withdrawals {
-        self.payload_attributes.withdrawals()
-    }
-
-    fn parent_beacon_block_root(&self) -> Option<B256> {
-        self.payload_attributes.parent_beacon_block_root()
-    }
-
     fn try_new(
         parent: B256,
         rpc_payload_attributes: Self::RpcPayloadAttributes,
@@ -122,53 +91,32 @@ impl PayloadBuilderAttributes for BerachainPayloadBuilderAttributes {
             validator_pubkey: rpc_payload_attributes.validator_pubkey,
         })
     }
-}
 
-pub fn berachain_payload_id(parent: &B256, attributes: &BerachainPayloadAttributes) -> PayloadId {
-    use sha2::{Digest, Sha256};
-
-    let mut hasher = Sha256::new();
-    hasher.update(parent.as_slice());
-    hasher.update(&attributes.payload_attributes.timestamp.to_be_bytes()[..]);
-    hasher.update(attributes.payload_attributes.prev_randao.as_slice());
-    hasher.update(attributes.payload_attributes.suggested_fee_recipient.as_slice());
-
-    if let Some(withdrawals) = &attributes.payload_attributes.withdrawals {
-        for withdrawal in withdrawals {
-            hasher.update(&withdrawal.index.to_be_bytes()[..]);
-            hasher.update(&withdrawal.validator_index.to_be_bytes()[..]);
-            hasher.update(withdrawal.address.as_slice());
-            hasher.update(&withdrawal.amount.to_be_bytes()[..]);
-        }
+    fn payload_id(&self) -> PayloadId {
+        self.payload_attributes.payload_id()
     }
 
-    if let Some(parent_beacon_block_root) = &attributes.payload_attributes.parent_beacon_block_root
-    {
-        hasher.update(parent_beacon_block_root.as_slice());
+    fn parent(&self) -> B256 {
+        self.payload_attributes.parent()
     }
 
-    if let Some(validator_pubkey) = &attributes.validator_pubkey {
-        hasher.update(validator_pubkey.0);
+    fn timestamp(&self) -> u64 {
+        self.payload_attributes.timestamp()
     }
 
-    let out = hasher.finalize();
-    PayloadId::new(out[..8].try_into().expect("sufficient length"))
-}
+    fn parent_beacon_block_root(&self) -> Option<B256> {
+        self.payload_attributes.parent_beacon_block_root()
+    }
 
-#[derive(Debug, Default, Clone)]
-pub struct BerachainPayloadTypes<N = EthPrimitives>(PhantomData<N>);
+    fn suggested_fee_recipient(&self) -> alloy_primitives::Address {
+        self.payload_attributes.suggested_fee_recipient()
+    }
 
-impl<N: reth_primitives::NodePrimitives> PayloadTypes for BerachainPayloadTypes<N> {
-    type ExecutionData = <EthPayloadTypes as PayloadTypes>::ExecutionData;
-    type BuiltPayload = <EthPayloadTypes as PayloadTypes>::BuiltPayload;
-    type PayloadAttributes = BerachainPayloadAttributes;
-    type PayloadBuilderAttributes = BerachainPayloadBuilderAttributes;
+    fn prev_randao(&self) -> B256 {
+        self.payload_attributes.prev_randao()
+    }
 
-    fn block_to_payload(
-        block: reth_primitives::SealedBlock<
-            <<Self::BuiltPayload as reth_payload_primitives::BuiltPayload>::Primitives as reth_primitives::NodePrimitives>::Block,
-        >,
-    ) -> Self::ExecutionData {
-        EthPayloadTypes::block_to_payload(block)
+    fn withdrawals(&self) -> &Withdrawals {
+        self.payload_attributes.withdrawals()
     }
 }
