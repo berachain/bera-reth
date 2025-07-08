be339f712f72dac0364e4ebe994bd9b0550e5607
Rez <rez@berachain.com>
2025-07-08 17:52:54 +1000

    Update receipt.rs

--- src/rpc/receipt.rs
+++ src/rpc/receipt.rs
@@ -1,12 +1,12 @@
 use crate::transaction::BerachainTxEnvelope;
-use alloy_consensus::transaction::TransactionMeta;
+use alloy_consensus::{ReceiptEnvelope, transaction::TransactionMeta};
 use alloy_eips::eip7840::BlobParams;
 use alloy_rpc_types_eth::TransactionReceipt;
-use reth_ethereum_primitives::{Receipt, TransactionSigned};
-use reth_rpc_eth_types::{EthReceiptBuilder, EthResult};
+use reth_ethereum_primitives::{Receipt, TxType};
+use reth_rpc_eth_types::{EthResult, receipt::build_receipt};
 
 pub struct BerachainEthReceiptBuilder {
-    inner: EthReceiptBuilder,
+    base: TransactionReceipt,
 }
 
 impl BerachainEthReceiptBuilder {
@@ -21,17 +21,26 @@ impl BerachainEthReceiptBuilder {
         all_receipts: &[Receipt],
         blob_params: Option<BlobParams>,
     ) -> EthResult<Self> {
-        match transaction {
-            BerachainTxEnvelope::Ethereum(tx) => {
-                let inner =
-                    EthReceiptBuilder::new(tx.into(), meta, receipt, all_receipts, blob_params)?;
-                Ok(Self { inner })
-            }
-        }
+        let base = build_receipt(
+            transaction,
+            meta,
+            receipt,
+            all_receipts,
+            blob_params,
+            |receipt_with_bloom| match receipt.tx_type {
+                TxType::Legacy => ReceiptEnvelope::Legacy(receipt_with_bloom),
+                TxType::Eip2930 => ReceiptEnvelope::Eip2930(receipt_with_bloom),
+                TxType::Eip1559 => ReceiptEnvelope::Eip1559(receipt_with_bloom),
+                TxType::Eip4844 => ReceiptEnvelope::Eip4844(receipt_with_bloom),
+                TxType::Eip7702 => ReceiptEnvelope::Eip7702(receipt_with_bloom),
+            },
+        )?;
+
+        Ok(Self { base })
     }
 
     /// Builds a receipt response from the base response body, and any set additional fields.
     pub fn build(self) -> TransactionReceipt {
-        self.inner.base
+        self.base
     }
 }
