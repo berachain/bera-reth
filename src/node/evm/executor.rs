bbc1773c975637d9bcda1e46f3cf939f217bb0d6
Rez <rez@berachain.com>
2025-09-09 15:53:18 +1000

    Follow new reth pattern in executor

--- src/node/evm/executor.rs
+++ src/node/evm/executor.rs
@@ -202,11 +202,10 @@ where
         }
 
         // Execute transaction.
-        // Extract needed data before transact() consumes tx
-        let tx_hash = tx.tx().trie_hash();
-        let tx_ref = tx.tx().clone();
-        let ResultAndState { result, state } =
-            self.evm.transact(tx).map_err(|err| BlockExecutionError::evm(err, tx_hash))?;
+        let ResultAndState { result, state } = self
+            .evm
+            .transact(&tx)
+            .map_err(|err| BlockExecutionError::evm(err, tx.tx().trie_hash()))?;
 
         if !f(&result).should_commit() {
             return Ok(None);
@@ -221,7 +220,7 @@ where
 
         // Push transaction changeset and calculate header bloom filter for receipt.
         self.receipts.push(self.receipt_builder.build_receipt(ReceiptBuilderCtx {
-            tx: &tx_ref,
+            tx: tx.tx(),
             evm: &self.evm,
             result,
             state: &state,
