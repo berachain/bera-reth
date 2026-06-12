851f9026c62b7f2fa0782be20ceb7d519f438d26
Cal Bera <calbera@berachain.com>
2026-06-12 04:09:54 -0600

    test(forks): update with new osaka fork time (#261)
    New fusaka fork time
    
    **Unix Timestamp: 1783526400
    GMT: Wed Jul 08 2026 16:00:00 GMT+0000**
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Chores**
    * Updated network configuration parameters for blockchain fork
    activation timing.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/chainspec/mod.rs
+++ src/chainspec/mod.rs
@@ -2315,10 +2315,10 @@ mod tests {
         assert_eq!(fork_id_prague1.next, 1759248000, "next fork should be Prague2");
         assert_eq!(fork_id_prague2.next, 1762164459, "next fork should be Prague3");
         assert_eq!(fork_id_prague3.next, 1762963200, "next fork should be Prague4");
-        assert_eq!(fork_id_prague4.next, 1782316800, "next fork should be Osaka");
+        assert_eq!(fork_id_prague4.next, 1783526400, "next fork should be Osaka");
         assert_eq!(fork_id_future.next, 0, "no next fork in far future");
 
-        // Expected fork hash values for mainnet (matching bera-geth test values)
+        // Expected fork hash values for mainnet
         assert_eq!(fork_id_genesis.hash, ForkHash([0xbb, 0x6c, 0x8b, 0xc0]));
         assert_eq!(fork_id_before_prague.hash, ForkHash([0xbb, 0x6c, 0x8b, 0xc0]));
         assert_eq!(fork_id_prague.hash, ForkHash([0x3f, 0x78, 0xb1, 0x27]));
@@ -2326,7 +2326,7 @@ mod tests {
         assert_eq!(fork_id_prague2.hash, ForkHash([0xcb, 0xbf, 0x6c, 0x9f]));
         assert_eq!(fork_id_prague3.hash, ForkHash([0x64, 0x94, 0xa1, 0x76]));
         assert_eq!(fork_id_prague4.hash, ForkHash([0x70, 0x1a, 0x09, 0x7f]));
-        assert_eq!(fork_id_future.hash, ForkHash([0x31, 0xd4, 0x38, 0xd0]));
+        assert_eq!(fork_id_future.hash, ForkHash([0xbb, 0x99, 0xd3, 0xeb]));
     }
 
     #[test]
